6ab910726502c670c9e36840db9fc61a128c10d0
Robert Stark <stark.r77@gmail.com>
2021-08-17 20:01:23 -0500

    first commit

---
+++ ElevenFiftyProjects/Assignments/CSSMain/main.cs/main.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace main.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i = 0;
+            string s = "HI!";
+            bool b = true;
+
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please enter age");
+            int entry = Int32.Parse(Console.ReadLine());
+
+            if (entry >= 18)
+            { Console.WriteLine("You are an adult");
+            
+            }
+
+            else if(entry < 18)
+            {
+                Console.WriteLine("You are a child.");
+            }
+        }
+    }
+}
